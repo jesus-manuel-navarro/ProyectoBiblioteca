b85452e83bb304e864e6eddffe0587cf2b23b456
Ana <anabarbera90@gmail.com>
2024-05-06 18:04:51 +0200

    Initial Commit

---
+++ ProyectoBiblioteca/ProyectoBibliotecaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoBibliotecaApp.swift
+//  ProyectoBiblioteca
+//
+//  Created by abarbera on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoBibliotecaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
